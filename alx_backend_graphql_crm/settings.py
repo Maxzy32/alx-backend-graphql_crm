eb46236eaf2f96bc81d059cbe65f9531c191b8c6
CaptaiRich <ahiakpormaxwell32@gmail.com>
2025-08-17 09:35:07 +0000

    bb

--- alx_backend_graphql_crm/settings.py
+++ alx_backend_graphql_crm/settings.py
@@ -31,13 +31,23 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+
+    # Third-party
+    "graphene_django",
+
+    # Local
+    "crm",
 ]
+
+GRAPHENE = {
+    "SCHEMA": "alx_backend_graphql_crm.schema.schema"  # points to schema.py
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
